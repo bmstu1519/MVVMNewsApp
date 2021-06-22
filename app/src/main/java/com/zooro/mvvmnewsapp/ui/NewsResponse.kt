c7d9db57da385d4dd2189667c8339e0a6b7bf45f
zOoro <rasengan21196@gmail.com>
2021-06-22 18:43:09 +0300

    add retrofit support:interface for request and RetrofitInstance

---
+++ app/src/main/java/com/zooro/mvvmnewsapp/ui/NewsResponse.kt
@@ -0,0 +1,7 @@
+package com.zooro.mvvmnewsapp.ui
+
+data class NewsResponse(
+    val articles: List<Article>,
+    val status: String,
+    val totalResults: Int
+)
---
+++ app/src/main/java/com/zooro/mvvmnewsapp/ui/Source.kt
@@ -0,0 +1,6 @@
+package com.zooro.mvvmnewsapp.ui
+
+data class Source(
+    val id: String,
+    val name: String
+)
