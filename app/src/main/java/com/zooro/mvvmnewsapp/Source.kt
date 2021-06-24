275cd1fc9758883f9b9d4e243ad5bdd449b1ceab
zOoro <rasengan21196@gmail.com>
2021-06-24 15:14:42 +0300

    moved to correct directory

---
+++ app/src/main/java/com/zooro/mvvmnewsapp/Source.kt
@@ -0,0 +1,6 @@
+package com.zooro.mvvmnewsapp
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/zooro/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.zooro.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "8d83e88e0c6040cb864ad1d7a3dbeda6"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
