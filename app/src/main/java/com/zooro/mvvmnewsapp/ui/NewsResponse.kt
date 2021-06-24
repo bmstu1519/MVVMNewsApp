44c335b4360272250c9174ead7b114fe3a0b6634
zOoro <rasengan21196@gmail.com>
2021-06-24 15:14:18 +0300

    moved to correct directory

--- app/src/main/java/com/zooro/mvvmnewsapp/ui/NewsResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.zooro.mvvmnewsapp.ui
-
-data class NewsResponse(
-    val articles: List<Article>,
-    val status: String,
-    val totalResults: Int
-)
--- app/src/main/java/com/zooro/mvvmnewsapp/ui/Source.kt
+++
@@ -1,6 +0,0 @@
-package com.zooro.mvvmnewsapp.ui
-
-data class Source(
-    val id: String,
-    val name: String
-)
