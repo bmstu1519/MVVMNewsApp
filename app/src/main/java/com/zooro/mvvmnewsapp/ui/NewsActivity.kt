a8c3095798262c08132eae822f02ebb6cb64e934
zOoro <rasengan21196@gmail.com>
2021-06-26 22:21:35 +0300

    default change

--- app/src/main/java/com/zooro/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/zooro/mvvmnewsapp/ui/NewsActivity.kt
@@ -2,16 +2,25 @@ package com.zooro.mvvmnewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.zooro.mvvmnewsapp.R
+import com.zooro.mvvmnewsapp.db.ArticleDatabase
+import com.zooro.mvvmnewsapp.repository.NewsRepository
 import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
+        val newsRepository = NewsRepository(ArticleDatabase.invoke(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
         bottomNavigationView.setupWithNavController(newsHavHostFragment.findNavController())
     }
 }
