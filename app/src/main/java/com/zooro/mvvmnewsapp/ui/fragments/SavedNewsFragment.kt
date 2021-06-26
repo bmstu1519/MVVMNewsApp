dbe6d695393975543eed63d62fff9722917d96c6
zOoro <rasengan21196@gmail.com>
2021-06-26 22:21:27 +0300

    default change

--- app/src/main/java/com/zooro/mvvmnewsapp/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/zooro/mvvmnewsapp/ui/fragments/SavedNewsFragment.kt
@@ -1,7 +1,18 @@
 package com.zooro.mvvmnewsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.zooro.mvvmnewsapp.R
+import com.zooro.mvvmnewsapp.ui.NewsActivity
+import com.zooro.mvvmnewsapp.ui.NewsViewModel
 
 class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
+
+    lateinit var viewModel : NewsViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+    }
 }
