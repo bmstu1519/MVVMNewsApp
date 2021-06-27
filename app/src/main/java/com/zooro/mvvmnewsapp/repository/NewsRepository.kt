ef7bcc9673b16da7299ef7ae166c41a089d996e9
zOoro <rasengan21196@gmail.com>
2021-06-28 00:35:14 +0300

    create coroutine for work with network request

--- app/src/main/java/com/zooro/mvvmnewsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/zooro/mvvmnewsapp/repository/NewsRepository.kt
@@ -1,8 +1,11 @@
 package com.zooro.mvvmnewsapp.repository
 
+import com.zooro.mvvmnewsapp.api.RetrofitInstance
 import com.zooro.mvvmnewsapp.db.ArticleDatabase
 
 class NewsRepository(
     val db: ArticleDatabase
 ) {
+    suspend fun  getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
 }
