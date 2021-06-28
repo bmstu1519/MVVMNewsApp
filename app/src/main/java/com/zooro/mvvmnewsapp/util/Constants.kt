822113b515c2a19a63fa0197aba331cbe93fab7c
zOoro <rasengan21196@gmail.com>
2021-06-28 17:33:12 +0300

    add SEARCH_NEWS_TIME_DELAY

--- app/src/main/java/com/zooro/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/zooro/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object{
         const val API_KEY = "8d83e88e0c6040cb864ad1d7a3dbeda6"
         const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
