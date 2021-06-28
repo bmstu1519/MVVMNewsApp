282150018db85fe7346c94b6a4602a1e7f83f568
zOoro <rasengan21196@gmail.com>
2021-06-28 23:18:11 +0300

    added webViewClient for display web article

--- app/src/main/java/com/zooro/mvvmnewsapp/ui/fragments/ArticleNewsFragment.kt
+++ app/src/main/java/com/zooro/mvvmnewsapp/ui/fragments/ArticleNewsFragment.kt
@@ -2,17 +2,26 @@ package com.zooro.mvvmnewsapp.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.zooro.mvvmnewsapp.R
 import com.zooro.mvvmnewsapp.ui.NewsActivity
 import com.zooro.mvvmnewsapp.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_article.*
 
 class ArticleNewsFragment : Fragment(R.layout.fragment_article) {
 
     lateinit var viewModel : NewsViewModel
+    val args: ArticleNewsFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        val article = args.article
+        webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(article.url)
+        }
     }
 }
