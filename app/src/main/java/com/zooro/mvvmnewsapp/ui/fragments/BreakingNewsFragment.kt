54534dd1d2e00e1ea66abd4770357e5d860467d0
zOoro <rasengan21196@gmail.com>
2021-07-01 17:29:54 +0300

    added logic to calculation last page in VM, notify observer, logic for scrollListener and setPadding for the last page (BreakingNewsFragment)

--- app/src/main/java/com/zooro/mvvmnewsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/zooro/mvvmnewsapp/ui/fragments/BreakingNewsFragment.kt
@@ -3,6 +3,7 @@ package com.zooro.mvvmnewsapp.ui.fragments
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.AbsListView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -12,6 +13,7 @@ import com.zooro.mvvmnewsapp.R
 import com.zooro.mvvmnewsapp.adapters.NewsAdapter
 import com.zooro.mvvmnewsapp.ui.NewsActivity
 import com.zooro.mvvmnewsapp.ui.NewsViewModel
+import com.zooro.mvvmnewsapp.util.Constants.Companion.QUERY_PAGE_SIZE
 import com.zooro.mvvmnewsapp.util.Resource
 import kotlinx.android.synthetic.main.fragment_breaking_news.*
 
@@ -42,7 +44,12 @@ class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
                 is Resource.Success -> {
                     hideProgressBar()
                     response.data?.let{ newsResponse ->
-                        newsAdapter.differ.submitList(newsResponse.articles)
+                        newsAdapter.differ.submitList(newsResponse.articles.toList())
+                        val totalPages = newsResponse.totalResults / QUERY_PAGE_SIZE + 2
+                        isLastPage = viewModel.breakingNewsPage == totalPages
+                        if (isLastPage){
+                            rvBreakingNews.setPadding(0, 0,0,0)
+                        }
                     }
                 }
                 is Resource.Error -> {
@@ -60,17 +67,58 @@ class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
 
     private fun hideProgressBar(){
         paginationProgressBar.visibility = View.INVISIBLE
+        isLoading = false
     }
 
     private fun showProgressBar(){
         paginationProgressBar.visibility = View.VISIBLE
+        isLoading = true
     }
 
+    var isLoading = false
+    var isLastPage = false
+    var isScrolling = false
+
+    val scrollListener = object : RecyclerView.OnScrollListener() {
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            if(newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                isScrolling = true
+            }
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+            //первый видимый элемент
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+            //общее количество видимых элементов
+            val visibleItemCount = layoutManager.childCount
+            //общее количество элементов
+            val totalItemCount = layoutManager.itemCount
+
+            val isNotLoadingAndNotLastPage =!isLoading && !isLastPage
+            val isAtLastItem = firstVisibleItemPosition + visibleItemCount >= totalItemCount
+            val isNotAtBeginning = firstVisibleItemPosition >= 0
+            val isTotalMoreThanVisible = totalItemCount >= QUERY_PAGE_SIZE
+            //разбивать на страницы или нет
+            val shouldPaginate = isNotLoadingAndNotLastPage && isAtLastItem && isNotAtBeginning &&
+                    isTotalMoreThanVisible && isScrolling
+
+            if (shouldPaginate){
+                viewModel.getBreakingNews("us")
+                isScrolling = false
+            }
+
+
+        }
+    }
     private fun setupRecyclerView(){
         newsAdapter = NewsAdapter()
         rvBreakingNews.apply {
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(activity)
+            addOnScrollListener(this@BreakingNewsFragment.scrollListener)
         }
     }
 }
