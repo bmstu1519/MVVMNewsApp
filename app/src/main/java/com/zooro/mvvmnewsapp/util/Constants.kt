5bb791f57f0ef7515df53498074e0b19df0df9eb
zOoro <rasengan21196@gmail.com>
2021-07-01 17:33:10 +0300

    added constant with max page size

--- app/src/main/java/com/zooro/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/zooro/mvvmnewsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val API_KEY = "8d83e88e0c6040cb864ad1d7a3dbeda6"
         const val BASE_URL = "https://newsapi.org/"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
