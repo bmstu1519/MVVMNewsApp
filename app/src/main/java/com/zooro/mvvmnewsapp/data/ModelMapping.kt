efb5cecbb77f66ab5257bee955115d3fced474d5
bmstu1519 <rasengan21196@gmail.com>
2025-01-10 10:35:51 +0300

    added mapper for list

--- app/src/main/java/com/zooro/mvvmnewsapp/data/ModelMapping.kt
+++ app/src/main/java/com/zooro/mvvmnewsapp/data/ModelMapping.kt
@@ -2,7 +2,9 @@ package com.zooro.mvvmnewsapp.data
 
 import com.zooro.mvvmnewsapp.data.db.ArticleDto
 import com.zooro.mvvmnewsapp.data.db.NewsSourceDto
+import com.zooro.mvvmnewsapp.data.network.NewsResponseDto
 import com.zooro.mvvmnewsapp.domain.model.Article
+import com.zooro.mvvmnewsapp.domain.model.NewsResponse
 import com.zooro.mvvmnewsapp.domain.model.NewsSource
 
 fun Article.toNetwork() = ArticleDto(
@@ -17,16 +19,28 @@ fun Article.toNetwork() = ArticleDto(
     urlToImage = urlToImage ,
 )
 
-fun ArticleDto.toLocal() = Article(
+fun ArticleDto.toDomain() = Article(
     id = id ,
     author = author ,
     content = content ,
     description = description ,
     publishedAt = publishedAt ,
-    newsSource = newsSource?.toLocal() ,
+    newsSource = newsSource?.toDomain() ,
     title = title ,
     url = url ,
     urlToImage = urlToImage ,
+)
+
+fun NewsResponse.toNetwork() = NewsResponseDto(
+    articles = articles,
+    status = status,
+    totalResults = totalResults,
+)
+
+fun NewsResponseDto.toDomain() = NewsResponse(
+    articles = articles,
+    status = status,
+    totalResults = totalResults,
 )
 
 fun NewsSource.toNetwork() = NewsSourceDto(
@@ -34,7 +48,37 @@ fun NewsSource.toNetwork() = NewsSourceDto(
     name = name,
 )
 
-fun NewsSourceDto.toLocal() = NewsSource(
+fun NewsSourceDto.toDomain() = NewsSource(
     id = id,
     name = name,
 )
+
+fun List<ArticleDto>.toDomain(): List<Article> =
+    map { articleDto ->
+        Article(
+            id = articleDto.id,
+            author = articleDto.author,
+            content = articleDto.content,
+            description = articleDto.description,
+            publishedAt = articleDto.publishedAt,
+            newsSource = articleDto.newsSource?.toDomain(),
+            title = articleDto.title,
+            url = articleDto.url,
+            urlToImage = articleDto.urlToImage
+        )
+    }
+
+fun List<Article>.toNetwork(): List<ArticleDto> =
+    map { article ->
+        ArticleDto(
+            id = article.id,
+            author = article.author,
+            content = article.content,
+            description = article.description,
+            publishedAt = article.publishedAt,
+            newsSource = article.newsSource?.toNetwork(),
+            title = article.title,
+            url = article.url,
+            urlToImage = article.urlToImage
+        )
+    }
