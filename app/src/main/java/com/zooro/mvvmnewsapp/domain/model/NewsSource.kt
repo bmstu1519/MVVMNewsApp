d648a462c78469b1b7fa794b44968a0b16067aa0
bmstu1519 <rasengan21196@gmail.com>
2025-01-11 19:14:57 +0300

    made Article parcelable

--- app/src/main/java/com/zooro/mvvmnewsapp/domain/model/NewsSource.kt
+++ app/src/main/java/com/zooro/mvvmnewsapp/domain/model/NewsSource.kt
@@ -1,6 +1,10 @@
 package com.zooro.mvvmnewsapp.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class NewsSource(
     val id: String,
     val name: String
-)
+): Parcelable
