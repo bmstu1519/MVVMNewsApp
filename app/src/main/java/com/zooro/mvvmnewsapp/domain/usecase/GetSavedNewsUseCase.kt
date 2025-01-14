d27305363fd121eafdd62578db9aec75f16b24f0
bmstu1519 <rasengan21196@gmail.com>
2025-01-14 17:38:26 +0300

    done GetSavedNewsUseCase

--- app/src/main/java/com/zooro/mvvmnewsapp/domain/usecase/GetSavedNewsUseCase.kt
+++ app/src/main/java/com/zooro/mvvmnewsapp/domain/usecase/GetSavedNewsUseCase.kt
@@ -1,4 +1,57 @@
 package com.zooro.mvvmnewsapp.domain.usecase
 
-class GetSavedNewsUseCase {
+import com.zooro.mvvmnewsapp.domain.model.Article
+import com.zooro.mvvmnewsapp.domain.repository.ArticleUseCaseRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.update
+
+data class SavedNewsState(
+    val isLoading: Boolean = false,
+    val savedNewsList: List<Article>? = null,
+    val errorMessage: String? = null
+)
+
+class GetSavedNewsUseCase(
+    private val articleUseCaseRepository: ArticleUseCaseRepository
+) {
+    private val _state = MutableStateFlow(SavedNewsState())
+    val state = _state.asStateFlow()
+
+    operator fun invoke(): Flow<List<Article>> =
+        articleUseCaseRepository.getSavedArticles()
+        .onStart {
+            updateState(isLoading = true)
+        }
+        .onEach { articles ->
+            updateState(
+                isLoading = false,
+                savedNewsList = articles
+            )
+        }
+        .catch { e ->
+            updateState(
+                isLoading = false,
+                savedNewsList = emptyList(),
+                errorMessage = e.message
+            )
+        }
+
+    private fun updateState(
+        isLoading: Boolean,
+        savedNewsList: List<Article>? = null,
+        errorMessage: String? = null
+    ) {
+        _state.update { currentState ->
+            currentState.copy(
+                isLoading = isLoading,
+                savedNewsList = savedNewsList ?: currentState.savedNewsList,
+                errorMessage = errorMessage ?: currentState.errorMessage,
+            )
+        }
+    }
 }
