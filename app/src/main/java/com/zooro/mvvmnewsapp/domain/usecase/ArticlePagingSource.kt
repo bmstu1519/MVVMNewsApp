37cdc0ce1ba38773ebf0d0dde3aad5d68025d1c3
bmstu1519 <rasengan21196@gmail.com>
2025-01-16 20:23:50 +0300

    paging source

---
+++ app/src/main/java/com/zooro/mvvmnewsapp/domain/usecase/ArticlePagingSource.kt
@@ -0,0 +1,45 @@
+package com.zooro.mvvmnewsapp.domain.usecase
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.zooro.mvvmnewsapp.data.toDomain
+import com.zooro.mvvmnewsapp.domain.model.Article
+import com.zooro.mvvmnewsapp.domain.repository.NewsRepository
+
+class ArticlePagingSource(
+    private val newsRepository: NewsRepository,
+    private val searchQuery: String
+) : PagingSource<Int, Article>() {
+
+    override fun getRefreshKey(state: PagingState<Int, Article>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
+        return try {
+            val page = params.key ?: 1
+
+            val response = newsRepository.searchNews(searchQuery, page)
+
+            response.fold(
+                onSuccess = { newsResponseDto ->
+                    val articles = newsResponseDto.articles.map { it.toDomain() }
+
+                    LoadResult.Page(
+                        data = articles,
+                        prevKey = if (page == 1) null else page - 1,
+                        nextKey = if (articles.isEmpty()) null else page + 1
+                    )
+                },
+                onFailure = { throwable ->
+                    LoadResult.Error(throwable)
+                }
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+}
