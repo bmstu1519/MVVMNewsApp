24d8dd24dc2d7a5e2d7e4939556b88c088308fa6
bmstu1519 <rasengan21196@gmail.com>
2025-01-23 12:12:09 +0300

    draft SearchNewsFragment

--- app/src/main/java/com/zooro/mvvmnewsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/zooro/mvvmnewsapp/ui/fragments/SearchNewsFragment.kt
@@ -5,28 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.zooro.mvvmnewsapp.R
 import com.zooro.mvvmnewsapp.databinding.FragmentSearchNewsBinding
-import com.zooro.mvvmnewsapp.domain.model.PaginationState
-import com.zooro.mvvmnewsapp.ui.NewsActivity
-import com.zooro.mvvmnewsapp.ui.adapters.NewsAdapter
-import com.zooro.mvvmnewsapp.ui.viewmodel.NewsViewModel
+import com.zooro.mvvmnewsapp.di.DependencyProvider
+import com.zooro.mvvmnewsapp.ui.adapters.NewsAdapterV2
+import com.zooro.mvvmnewsapp.ui.viewmodel.SearchNewsViewModel
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
 
-    private lateinit var viewModel: NewsViewModel
-    private lateinit var newsAdapter: NewsAdapter
-    private var searchQuery: String = ""
+    private lateinit var viewModel: SearchNewsViewModel
+    private lateinit var newsAdapter: NewsAdapterV2
     private lateinit var binding: FragmentSearchNewsBinding
 
     override fun onCreateView(
@@ -40,15 +36,13 @@ class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = (activity as NewsActivity).viewModel
+        val viewModelFactory = DependencyProvider.viewModelFactory
+        viewModel = ViewModelProvider(this, viewModelFactory)[SearchNewsViewModel::class.java]
+
         setupRecyclerView()
-        setupPagination()
         setupClickListeners()
         setupSearchListener()
         observeUiState()
-
-//        viewModel.searchNews(true)
-        viewModel.resetPaginationState()
     }
 
     private fun setupClickListeners() {
@@ -65,30 +59,31 @@ class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
 
     private fun setupSearchListener() {
         binding.etSearch.addTextChangedListener { editable ->
-            searchQuery = editable?.toString() ?: ""
-            viewModel.searchNews(searchQuery)
+            viewModel.updateQuery(editable?.toString() ?: "")
         }
     }
+
 
     private fun observeUiState() {
         viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.state.collect { uiState ->
-                    handleError(uiState.errorMessage)
-                    handlePagination(uiState.paginationState)
+            viewModel.uiState.collectLatest { state ->
+                when {
+                    state.isLoading -> {
+//                        binding.progressBar.isVisible = true
+//                        binding.errorLayout.isVisible = false
+                    }
+                    state.errorMessage != null -> {
+//                        binding.progressBar.isVisible = false
+//                        binding.errorLayout.isVisible = true
+//                        binding.errorMessage.text = state.errorMessage
+                        handleError(state.errorMessage)
+                    }
+                    state.data != null -> {
+//                        binding.progressBar.isVisible = false
+//                        binding.errorLayout.isVisible = false
+                        newsAdapter.submitData(state.data)
+                    }
                 }
-            }
-        }
-    }
-
-    private fun handlePagination(paginationState: PaginationState?) {
-        paginationState?.let { state ->
-            newsAdapter.differ.submitList(state.items)
-
-            binding.paginationProgressBar.isVisible = state.isLoading
-
-            if (state.isLastPage) {
-                binding.rvSearchNews.setPadding(0, 0, 0, 0)
             }
         }
     }
@@ -97,37 +92,20 @@ class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
         errorMessage?.let {
             Toast.makeText(
                 activity,
-                "You have requested too many results.\n" +
-                        "Developer accounts are limited to a max of 100 results.",
+                errorMessage,
+//                "You have requested too many results.\n" +
+//                        "Developer accounts are limited to a max of 100 results.",
                 Toast.LENGTH_LONG
             ).show()
         }
     }
-
-    private fun setupPagination() {
-        binding.rvSearchNews.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-
-                if (dy > 0) {
-                    val layoutManager = recyclerView.layoutManager as LinearLayoutManager
-                    val visibleItemCount = layoutManager.childCount
-                    val totalItemCount = layoutManager.itemCount
-                    val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
-
-                    val shouldLoadMore = visibleItemCount + firstVisibleItemPosition >= totalItemCount - 5
-                    if (shouldLoadMore) {
-                        viewModel.searchNews(searchQuery)
-                    }
-                }
-            }
-        })
-    }
-
     private fun setupRecyclerView() {
-        newsAdapter = NewsAdapter()
+        newsAdapter = NewsAdapterV2()
         binding.rvSearchNews.apply {
             adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+        binding.rvSearchNews.apply {
             layoutManager = LinearLayoutManager(activity)
         }
     }
