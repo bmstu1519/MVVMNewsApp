451dbe5503f00b092dab7daec1572eff1e3d3f62
bmstu1519 <rasengan21196@gmail.com>
2025-01-24 15:12:29 +0300

    make fun with update and copy

--- app/src/main/java/com/zooro/mvvmnewsapp/ui/viewmodel/SearchNewsViewModel.kt
+++ app/src/main/java/com/zooro/mvvmnewsapp/ui/viewmodel/SearchNewsViewModel.kt
@@ -7,6 +7,7 @@ import androidx.paging.cachedIn
 import com.zooro.mvvmnewsapp.domain.model.Article
 import com.zooro.mvvmnewsapp.domain.repository.NewsApiRepository
 import com.zooro.mvvmnewsapp.ui.util.UiState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
@@ -14,7 +15,9 @@ import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.filterNot
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlin.coroutines.cancellation.CancellationException
 
 class SearchNewsViewModel(
@@ -38,7 +41,7 @@ class SearchNewsViewModel(
         viewModelScope.launch {
             currentSearchQuery
                 .onStart {
-                    _state.value = UiState(
+                    updateState(
                         isLoading = true
                     )
                 }
@@ -47,18 +50,20 @@ class SearchNewsViewModel(
                 .filter { it.length >= 3 }
                 .collectLatest { query ->
                     try {
-                        pagingRepository.getSearchNews(query)
-                            .cachedIn(viewModelScope)
-                            .collect { pagingData ->
-                                _state.value = UiState(
-                                    data = pagingData,
-                                    isLoading = false,
-                                    errorMessage = null
-                                )
-                            }
+                        withContext(Dispatchers.IO) {
+                            pagingRepository.getSearchNews(query)
+                                .cachedIn(viewModelScope)
+                                .collect { pagingData ->
+                                    updateState(
+                                        data = pagingData,
+                                        isLoading = false,
+                                        errorMessage = null
+                                    )
+                                }
+                        }
                     } catch (e: Exception) {
                         if (e !is CancellationException) {
-                            _state.value = UiState(
+                            updateState(
                                 data = null,
                                 isLoading = false,
                                 errorMessage = e.localizedMessage
@@ -66,6 +71,22 @@ class SearchNewsViewModel(
                         }
                     }
                 }
+        }
+    }
+
+    private suspend fun updateState(
+        data: PagingData<Article>? = null,
+        isLoading: Boolean = false,
+        errorMessage: String? = null
+    ) {
+        withContext(Dispatchers.Main) {
+            _state.update { current ->
+                current.copy(
+                    data = data ?: current.data,
+                    isLoading = isLoading,
+                    errorMessage = errorMessage
+                )
+            }
         }
     }
 }
