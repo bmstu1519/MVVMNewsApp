75e16c7f6da43f9d4bcde9c280b3929f9ba4bcc3
bmstu1519 <rasengan21196@gmail.com>
2025-01-25 12:17:05 +0300

    removed useless entities

--- app/src/main/java/com/zooro/mvvmnewsapp/ui/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/zooro/mvvmnewsapp/ui/viewmodel/ViewModelFactory.kt
@@ -2,25 +2,36 @@ package com.zooro.mvvmnewsapp.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.zooro.mvvmnewsapp.domain.usecase.GetArticleUseCase
-import com.zooro.mvvmnewsapp.domain.usecase.GetBreakingNewsUseCase
-import com.zooro.mvvmnewsapp.domain.usecase.GetSavedNewsUseCase
-import com.zooro.mvvmnewsapp.domain.usecase.GetSearchNewsUseCase
+import com.zooro.mvvmnewsapp.domain.repository.ArticleRepository
+import com.zooro.mvvmnewsapp.domain.repository.NetworkHelperRepository
+import com.zooro.mvvmnewsapp.domain.repository.NewsApiRepository
 
 class ViewModelFactory(
-    private val getBreakingNewsUseCase: GetBreakingNewsUseCase,
-    private val getSearchNewsUseCase: GetSearchNewsUseCase,
-    private val getArticleUseCase: GetArticleUseCase,
-    private val getSavedNewsUseCase: GetSavedNewsUseCase
+    private val networkHelper: NetworkHelperRepository,
+    private val pagingRepository: NewsApiRepository,
+    private val articleRepository: ArticleRepository
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
-            modelClass.isAssignableFrom(NewsViewModel::class.java) -> {
-                NewsViewModel(
-                    getBreakingNewsUseCase,
-                    getSearchNewsUseCase,
-                    getArticleUseCase,
-                    getSavedNewsUseCase
+            modelClass.isAssignableFrom(SearchNewsViewModel::class.java) -> {
+                SearchNewsViewModel(
+                    pagingRepository
+                ) as T
+            }
+
+            modelClass.isAssignableFrom(BreakingNewsViewModel::class.java) -> {
+                BreakingNewsViewModel(
+                    pagingRepository
+                ) as T
+            }
+            modelClass.isAssignableFrom(SavedNewsViewModel::class.java) -> {
+                SavedNewsViewModel(
+                    articleRepository
+                ) as T
+            }
+            modelClass.isAssignableFrom(ArticleNewsViewModel::class.java) -> {
+                ArticleNewsViewModel(
+                    articleRepository
                 ) as T
             }
 
